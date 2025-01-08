739580505e4eef5f78ea21ee4733346e294798d0
Abishek_Prakash <abishek.pksh@gmail.com>
2025-01-08 16:15:31 +0000

    Trained a model using SMOTE again, but with better data preprocessing as previously, we used SMOTE on the entire dataset but now I only used it on the training data as SMOTE should not be applied to test and val data to make it reflect the original class distribution to evaluate real-world performance accurately. So, changed time_datamodule.py as I split the train, val and test sets.

--- classification-clean/src/datamodules/time_datamodule.py
+++ classification-clean/src/datamodules/time_datamodule.py
@@ -41,25 +41,22 @@ class TimeDataModule(L.LightningDataModule):
     
     def prepare_data(self):
         if not self.train_dataset and not self.val_dataset and not self.test_dataset:
-            # load the data
-            data = np.load(os.path.join(self.data_dir, "signals.npy"))
-            target = np.load(os.path.join(self.data_dir, "targets.npy"))
-            if self.split_ratio != None:
-                num_train = math.ceil(data.shape[0] * self.split_ratio[0])
-                num_val = math.ceil(data.shape[0] * self.split_ratio[1])
-                
-                train_data, train_target = data[:num_train], target[:num_train]
-                val_data, val_target = data[num_train:num_train+num_val], target[num_train:num_train+num_val]
-                test_data, test_target = data[num_train+num_val:], target[num_train+num_val:]
-            
-            normalizer = Normalizer(norm_type='standardization')
+            # Load preprocessed data
+            train_data = np.load(os.path.join(self.data_dir, "train_signals.npy"))
+            train_target = np.load(os.path.join(self.data_dir, "train_targets.npy"))
+            val_data = np.load(os.path.join(self.data_dir, "val_signals.npy"))
+            val_target = np.load(os.path.join(self.data_dir, "val_targets.npy"))
+            test_data = np.load(os.path.join(self.data_dir, "test_signals.npy"))
+            test_target = np.load(os.path.join(self.data_dir, "test_targets.npy"))
+
+            # Normalize the data
+            normalizer = Normalizer(norm_type="standardization")
             normalizer.fit(train_data)
 
-            # normalize the data and transpose it to [num_samples, features, sequence]
             train_data = normalizer.transform(train_data).transpose(0, 2, 1)
             val_data = normalizer.transform(val_data).transpose(0, 2, 1)
             test_data = normalizer.transform(test_data).transpose(0, 2, 1)
-            
+
             self.train_dataset = TSDataset(train_data, train_target)
             self.val_dataset = TSDataset(val_data, val_target)
             self.test_dataset = TSDataset(test_data, test_target)
