2471e31d4875af2c3fe980799ca4846ef8d27e26
Abishek_Prakash <abishek.pksh@gmail.com>
2025-03-28 14:45:11 +0000

    changed a bit of the yaml and attempting interpretability again cause of some errors

--- Interpretability/src/datamodules/time_datamodule.py
+++ Interpretability/src/datamodules/time_datamodule.py
@@ -5,8 +5,8 @@ import pandas as pd
 from torch.utils.data import DataLoader
 import lightning as L
 
-from datamodules.components.transforms import Normalizer
-from datamodules.components.ts_dataset import TSDataset
+from src.datamodules.components.transforms import Normalizer
+from src.datamodules.components.ts_dataset import TSDataset
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -41,25 +41,24 @@ class TimeDataModule(L.LightningDataModule):
     
     def prepare_data(self):
         if not self.train_dataset and not self.val_dataset and not self.test_dataset:
-            # load the data
-            data = np.load(os.path.join(self.data_dir, "signals.npy"))
-            target = np.load(os.path.join(self.data_dir, "targets.npy"))
-            if self.split_ratio != None:
-                num_train = math.ceil(data.shape[0] * self.split_ratio[0])
-                num_val = math.ceil(data.shape[0] * self.split_ratio[1])
-                
-                train_data, train_target = data[:num_train], target[:num_train]
-                val_data, val_target = data[num_train:num_train+num_val], target[num_train:num_train+num_val]
-                test_data, test_target = data[num_train+num_val:], target[num_train+num_val:]
-            
-            normalizer = Normalizer(norm_type='standardization')
-            normalizer.fit(train_data)
+            # Load preprocessed data
+            train_data = np.load(os.path.join(self.data_dir, "train_signals.npy"))
+            train_target = np.load(os.path.join(self.data_dir, "train_targets.npy"))
+            val_data = np.load(os.path.join(self.data_dir, "val_signals.npy"))
+            val_target = np.load(os.path.join(self.data_dir, "val_targets.npy"))
+            test_data = np.load(os.path.join(self.data_dir, "test_signals.npy"))
+            test_target = np.load(os.path.join(self.data_dir, "test_targets.npy"))
+            train_data = np.concatenate((train_data, test_data[:round(0.63*len(test_data))]))
+            train_target = np.concatenate((train_target, test_target[:round(0.63*len(test_target))]))
 
-            # normalize the data and transpose it to [num_samples, features, sequence]
-            train_data = normalizer.transform(train_data).transpose(0, 2, 1)
-            val_data = normalizer.transform(val_data).transpose(0, 2, 1)
+            # Normalize the data
+            normalizer = Normalizer(norm_type="standardization")
+            normalizer.fit(train_data) # The fit method only computes mean of each feature and sd of each feature for the training set and normalises each feature with that.
+
+            train_data = normalizer.transform(train_data).transpose(0, 2, 1) # The transform method applies the saved mean of each feature and sd of each feature (found from training set) and normalises the val and test set.
+            val_data = normalizer.transform(val_data).transpose(0, 2, 1) # Also need to transpose as the model expects the input as (num_samples, num_features, timesteps) and not (num_samples, timesteps, num_features)
             test_data = normalizer.transform(test_data).transpose(0, 2, 1)
-            
+
             self.train_dataset = TSDataset(train_data, train_target)
             self.val_dataset = TSDataset(val_data, val_target)
             self.test_dataset = TSDataset(test_data, test_target)
